35b4ca499bd2f275968961236d552030e482a442
Daniel Müller <deso@posteo.net>
2024-07-16 06:51:15 -0700

    cli: Remove additional context from clap errors
    From the looks of it, clap reported errors are basically meant to stand
    on their own. In our case, the additional "failed to parse program
    arguments" context we add doesn't help at all and just causes the
    resulting error report to look awkward:
    
      > Error: failed to parse program arguments
      >
      > Caused by:
      >     error: unexpected argument '-a' found
      >
      >     Usage: shavecli [OPTIONS] <COMMAND>
      >
      >     For more information, try '--help'.
    
    Omit this context for a slightly improved experience.

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -80,7 +80,7 @@ where
         print!("{}", err);
         return Ok(())
       },
-      _ => return Err(err).context("failed to parse program arguments"),
+      _ => return Err(err.into()),
     },
   };
 
